b796caafa8a83d10d63e8e47ef5126036084ec0d
Ksenia Skirpichnikova <k.skirpichnikova@gmail.com>
2019-09-14 10:22:32 +0200

    update basics.sh (path for list_of_contents.txt)

--- exam/Skirpichnikova_Ksenia/IT/basics.sh
+++ exam/Skirpichnikova_Ksenia/IT/basics.sh
@@ -1,7 +1,7 @@
 cd
-cd exam/Skirpichnikova_Ksenia/IT
+cd ba-pre-session-2019/exam/Skirpichnikova_Ksenia/IT
 mkdir made_by_my_script
 cd ..
 ls
-ls>list_of_contents.txt
+ls>IT/made_by_my_script/list_of_contents.txt
 
